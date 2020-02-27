b84bce9081db8e52b4d01355afd2c9f38ce05689
dmitalek <61025540+dmitalek@users.noreply.github.com>
2020-02-27 18:55:46 +0300

    Add files via upload

---
+++ JS/zad_6.8.js
@@ -0,0 +1,29 @@
+let fill = 0;
+let bar = $("#progressBar").width();;
+$(document).ready() ;
+
+function but1 () {
+    if (fill < 100) fill ++;
+    if (fill > 100) fill = 100, alert("Пора ползти домой!");
+    $("#my-bar").width(fill+"%");
+    console.log (fill);
+}
+function but2 () {
+    if (fill < 100) fill += 3;
+    if (fill > 100) fill = 100, alert("Пора ползти домой!");
+    $("#my-bar").width(fill+"%");
+    console.log(fill);
+}
+function but3() {
+    if (fill < 100) fill += 7;
+    if (fill > 100) fill = 100, alert("Пора ползти домой!");
+    $("#my-bar").width(fill+"%");
+    console.log (fill);
+    
+}
+$("#button-1").click(but1);
+
+$("#button-3").click(but2);
+
+$("#button-7").click(but3);
+    
